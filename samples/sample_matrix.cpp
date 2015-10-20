e317a72c08036c6126c8f33dbb9eefe3e59cb9c0
Wropwtr <buldakov.95@mail.ru>
2015-10-20 17:36:00 +0300

    Add my_menu() function

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -9,24 +9,50 @@
 #include "utmatrix.h"
 //---------------------------------------------------------------------------
 
+void my_menu();
+
+void default_menu(){
+	TMatrix<int> a(5), b(5), c(5);
+	int i, j;
+
+	setlocale(LC_ALL, "Russian");
+	cout << "Тестирование программ поддержки представления треугольных матриц"
+		<< endl;
+	for (i = 0; i < 5; i++)
+	for (j = i; j < 5; j++)
+	{
+		a[i][j] = i * 10 + j;
+		b[i][j] = (i * 10 + j) * 100;
+	}
+	c = a + b;
+	cout << "Matrix a = " << endl << a << endl;
+	cout << "Matrix b = " << endl << b << endl;
+	cout << "Matrix c = a + b" << endl << c << endl;
+	getchar();
+}
+
 void main()
 {
-  TMatrix<int> a(5), b(5), c(5);
-  int i, j;
-
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование программ поддержки представления треугольных матриц"
-    << endl;
-  for (i = 0; i < 5; i++)
-    for (j = i; j < 5; j++ )
-    {
-      a[i][j] =  i * 10 + j;
-      b[i][j] = (i * 10 + j) * 100;
-    }
-  c = a + b;
-  cout << "Matrix a = " << endl << a << endl;
-  cout << "Matrix b = " << endl << b << endl;
-  cout << "Matrix c = a + b" << endl << c << endl;
-  getchar();
+	default_menu();
+	return;
 }
 //---------------------------------------------------------------------------
+
+void my_menu(){
+
+	TMatrix<int> a(5), b(5), c(5);
+	int j, i = 1;
+
+	while (i != 0){
+
+		cout << "Menu" << endl;
+		cout << "1 - Enter matrix A" << endl;
+		cout << "2 - Enter matrix B" << endl;
+		cout << "3 - Swap A and B" << endl;
+		cout << "4 - A - B" << endl;
+		cout << "4 - A + B\n>" << endl;
+		cin >> i;
+
+	}
+}
+
