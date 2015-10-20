e6d6651a9a18b87f8c24816b3eff99006a116356
Wropwtr <buldakov.95@mail.ru>
2015-10-20 17:58:52 +0300

    Add my_menu() function implementation

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -9,6 +9,8 @@
 #include "utmatrix.h"
 //---------------------------------------------------------------------------
 
+#define Type int
+
 void my_menu();
 
 void default_menu(){
@@ -33,14 +35,15 @@ void default_menu(){
 
 void main()
 {
-	default_menu();
+	//default_menu();
+	my_menu();
 	return;
 }
 //---------------------------------------------------------------------------
 
 void my_menu(){
 
-	TMatrix<int> a(5), b(5), c(5);
+	TMatrix<int> a, b;
 	int j, i = 1;
 
 	while (i != 0){
@@ -50,9 +53,84 @@ void my_menu(){
 		cout << "2 - Enter matrix B" << endl;
 		cout << "3 - Swap A and B" << endl;
 		cout << "4 - A - B" << endl;
-		cout << "4 - A + B\n>" << endl;
+		cout << "5 - A + B>" << endl;
+		cout << "6 - Zero both matrix>" << endl;
+		cout << "7 - Print A>" << endl;
+		cout << "8 - Print B>" << endl;
+		cout << "0 - EXIT>" << endl;
 		cin >> i;
+		cout << endl;
 
+		switch (i){
+		case 1:
+		{
+				  int sizeA = 0;
+				  cout << "Enter size of matrix\n";
+				  cin >> sizeA;  // try mb
+				  a = TMatrix<Type>(sizeA);
+				  cout << "Enter matrix:\n";
+				  cin >> a;
+
+				  break;
+		}
+		case 2:
+		{
+				  int sizeA = 0;
+				  cout << "Enter size of matrix\n";
+				  cin >> sizeA;  // try mb
+				  b = TMatrix<Type>(sizeA);
+				  cout << "Enter matrix:\n";
+				  cin >> b;
+
+				  break;
+		}
+		case 3:
+		{
+				  TMatrix<Type> buf = a;
+				  a = b;
+				  b = buf;
+
+				  break;
+		}
+		case 4:
+		{
+				  TMatrix<Type> buf = a - b;
+				  cout << buf;
+
+				  break;
+		}
+		case 5:
+		{
+				  TMatrix<Type> buf = a + b;
+				  cout << buf;
+
+				  break;
+		}
+		case 6:
+		{
+				  a = TMatrix<Type>(a.GetSize());
+				  b = TMatrix<Type>(b.GetSize());
+
+				  break;
+		}
+		case 7:
+		{
+				  cout << a;
+				  break;
+		}
+		case 8:
+		{
+				  cout << b;
+				  break;
+		}
+		case 0:
+			return;
+		default:
+			continue;
+		}
+		cout << endl;
 	}
+
+	return;
 }
 
